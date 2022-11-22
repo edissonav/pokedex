8bf481de4d972de1bbdc8a23cf1f875ece2ee2ca
RicardoLS90 <108825418+RicardoLS90@users.noreply.github.com>
2022-11-22 18:40:04 +0000

    actualizado

--- src/components/Pokedex.jsx
+++ src/components/Pokedex.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { changename } from '../store/slices/name.slice';
 import Pokemoncard from './Pokemoncard';
+import '../pokedex.css'
 
 const Pokedex = () => {
 
@@ -15,22 +16,22 @@ const Pokedex = () => {
 
     const [pokemontype, setPokemontype] = useState([])
 
-    const [page, setPage]=useState(1)
-    const pokemonsperpage=20
-    const lastindex=page*pokemonsperpage
-    const firstindex=lastindex-pokemonsperpage
+    const [page, setPage] = useState(1)
+    const pokemonsperpage = 20
+    const lastindex = page * pokemonsperpage
+    const firstindex = lastindex - pokemonsperpage
 
-    const pokemonpaginated= pokemons?.slice(firstindex, lastindex)
+    const pokemonpaginated = pokemons?.slice(firstindex, lastindex)
 
-    const totalpages= Math.ceil(pokemons?.length /pokemonsperpage) 
+    const totalpages = Math.ceil(pokemons?.length / pokemonsperpage)
 
-    const numbers=[]
-    for(let i=1; i<= totalpages; i++){
+    const numbers = []
+    for (let i = 1; i <= totalpages; i++) {
         numbers.push(i)
-    } 
+    }
 
     useEffect(() => {
-        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154') 
+        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154')
             .then(res => setPokemons(res.data.results));
 
         axios.get('https://pokeapi.co/api/v2/type/')
@@ -38,6 +39,11 @@ const Pokedex = () => {
 
     }, [])
     // console.log(pokemontype);
+
+    const allPokemons = () => {
+        axios.get('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1154')
+            .then(res => setPokemons(res.data.results));
+    }
 
     const navigate = useNavigate();
 
@@ -47,59 +53,73 @@ const Pokedex = () => {
         navigate(`/pokedex/${pokemonName.toLowerCase()}`)
     }
     const filterType = (e) => {
-        axios.get(e.target.value)
-        .then(res=> setPokemons(res.data.pokemon))
+        if ((e.target.value) === "All Pokemons") { allPokemons() }
+        else {
+            axios.get(e.target.value)
+                .then(res => setPokemons(res.data.pokemon))
+        }
     }
 
-    const [suggestions, setSuggestions]=useState([])
+    const [suggestions, setSuggestions] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
 
-        const filteredpokemons= pokemons.filter((pokemon)=> (pokemon.name === pokemonName))
+        const filteredpokemons = pokemons.filter((pokemon) => (pokemon.name === pokemonName))
         setSuggestions(filteredpokemons)
         // axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
         // .then(res=>setSuggestions(res.data))
-    },[pokemonName])
+    }, [pokemonName])
 
     // console.log(suggestions);
     return (
-        <div>
-            <h1>Welcome {userName}</h1>
-
-            <div>
-                <input type="text" placeholder='serch pokemon' value={pokemonName} onChange={e => setPokemonName(e.target.value)} />
-                <button onClick={searchpokemon}>Search pokemon</button>
-                <select onChange={filterType} name="" id="">
+        <div className='pokedex-container'>
+            <div className='search-container'>
+                <h1 className='pokedex-tittle'>Welcome {userName}</h1>
+                <select className='select-type' onChange={filterType} name="" id="">
+                    <option value='All Pokemons'>All Pokemons</option>
                     {pokemontype.map(type => (
                         <option key={type.url} value={type.url}>{type.name}</option>
                     ))
 
                     }
                 </select>
-                {
+                <div className='search-name'>
+                    <input className='search-input' type="text" placeholder='Search Pokemon' value={pokemonName} onChange={e => setPokemonName(e.target.value)} />
+                    <button className='search-button' onClick={searchpokemon}>Search</button>
+                </div>
+                {/* {
                     suggestions.map(suggestion=>(
                         <li key={suggestion.url} onClick={searchpokemon}>{suggestion.name}</li>
                     ))
-                }
+                } */}
+
+                <h2 className='pokedex-tittle' >Pokemon List</h2>
             </div>
 
-            <h2>Pokemons</h2>
-            <div>
-                    <button disabled={page===1} onClick={()=> setPage(page-1)}>prev page</button>
 
-                    {numbers.map(number=>(
-                        <button onClick={()=>setPage(number) }>{number}</button>
-                    ))}
-                    <button disabled={page=== totalpages} onClick={()=>setPage(page+1)}>next page</button>
-                </div>
+
             <div className='pokemons'>
-                
+
                 {
                     pokemonpaginated.map(pokemon => (
-                        <Pokemoncard url={pokemon.url ? pokemon.url : pokemon.pokemon.url} key={pokemon.url? pokemon.url : pokemon.pokemon.url} />
+                        <Pokemoncard url={pokemon.url ? pokemon.url : pokemon.pokemon.url} key={pokemon.url ? pokemon.url : pokemon.pokemon.url} />
                     ))
                 }
 
+
+            </div>
+            <div className='page-container'>
+                <div className='prev-button-container'>
+                <button className='prev-button  next-prev' disabled={page === 1} onClick={() => setPage(page - 1)}>prev page</button>
+                </div>
+                <div className='page-button-container'>
+                {numbers.map(number => (
+                    <button className='page-button' key={number} onClick={() => setPage(number)}>{number} </button>
+                ))}
+                </div>
+                <div className='next-button-container'>
+                <button className='next-button next-prev ' disabled={page === totalpages} onClick={() => setPage(page + 1)}>next page</button>
+                </div>
             </div>
         </div>
     );
--- src/components/Pokemoncard.jsx
+++ src/components/Pokemoncard.jsx
@@ -3,23 +3,33 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
-const Pokemoncard = ({url}) => {
+const Pokemoncard = ({ url }) => {
 
-    const [pokemon, setPokemon]=useState({})
+    const [pokemon, setPokemon] = useState({})
 
     useEffect(
-        ()=>{
-            axios.get(url).then(res=> setPokemon(res.data))
-        },[]
+        () => {
+            axios.get(url).then(res => setPokemon(res.data))
+        }, []
     )
-    // console.log(pokemon);
+    console.log(pokemon);
     return (
         <div className='pokemon-card'>
-            <Link to={`/pokedex/${pokemon.id}`}>
-            
-            <h1>{pokemon.name}</h1>
-            <img className='pokemon-img-pokedex' src={pokemon.sprites?.other.home.front_default} alt="" />
-            <h2>type:{pokemon?.types?.[0].type.name}</h2>
+            <Link to={`/pokedex/${pokemon.id}`} className='card-container' >
+                <div className='info-container'>
+                    <div className='pokemon-name'>
+                        <h1>{pokemon.name}</h1>
+                    </div>
+                    <div className='pokemon-stats'>
+                        <p><b> type:</b>  {pokemon?.types?.[0].type.name}</p>
+                        <p><b> hp: </b>{pokemon?.stats?.[0].base_stat}</p>
+                        <p><b> Attack: </b>{pokemon?.stats?.[1].base_stat}</p>
+                        <p><b> Defense: </b>{pokemon?.stats?.[2].base_stat}</p>
+                        <p><b> Speed :</b>{pokemon?.stats?.[5].base_stat}</p>
+                    </div>
+                </div>
+                <img className='pokemon-img-pokedex' src={pokemon.sprites?.other.home.front_default} alt="" />
+
             </Link>
         </div>
     );
